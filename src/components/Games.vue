bf9e516ab75b48f8cb0841743bbdd88673ac99e2
G0SSZILLA <gossman15@gmail.com>
2020-06-23 10:55:15 -0600

    games

---
+++ src/components/Games.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="Games">
+
+
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'Games',
+    data(){
+        return {}
+    },
+    computed:{},
+    methods:{},
+    components:{}
+}
+</script>
+
+
+<style scoped>
+
+</style>
