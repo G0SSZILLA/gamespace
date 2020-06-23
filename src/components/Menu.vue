02529468c59a87a52eb92130006d1bea205f30d8
G0SSZILLA <gossman15@gmail.com>
2020-06-23 10:47:32 -0600

    right menu added

---
+++ src/components/Menu.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="Menu">
+
+
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'Menu',
+    data(){
+        return {}
+    },
+    computed:{},
+    methods:{},
+    components:{}
+}
+</script>
+
+
+<style scoped>
+
+</style>
