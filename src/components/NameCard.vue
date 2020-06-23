c59f372329f712d39f7a22fe6bedb1a0e613710f
G0SSZILLA <gossman15@gmail.com>
2020-06-23 10:54:28 -0600

    nameCard

---
+++ src/components/NameCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="NameCard">
+
+
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'NameCard',
+    data(){
+        return {}
+    },
+    computed:{},
+    methods:{},
+    components:{}
+}
+</script>
+
+
+<style scoped>
+
+</style>
