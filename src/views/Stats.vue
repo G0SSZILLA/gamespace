cbc8efb066b0ebcfdea2cb7a9c9fd7ad86588db9
G0SSZILLA <gossman15@gmail.com>
2020-06-23 10:45:01 -0600

    added stats view

---
+++ src/views/Stats.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="Stats">
+
+
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'Stats',
+    data(){
+        return {}
+    },
+    computed:{},
+    methods:{},
+    components:{}
+}
+</script>
+
+
+<style scoped>
+
+</style>
