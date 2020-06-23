5e8376361b172e8e599e01eb1d5decc8c0918f7e
G0SSZILLA <gossman15@gmail.com>
2020-06-23 10:48:20 -0600

    added social menu

---
+++ src/components/SocialMenu.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="SocialMenu">
+
+
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'SocialMenu',
+    data(){
+        return {}
+    },
+    computed:{},
+    methods:{},
+    components:{}
+}
+</script>
+
+
+<style scoped>
+
+</style>
